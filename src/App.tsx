07787d3bfcce114bd294792e8d638c91aca995ac
Raul-Docs <raul.cal.bar@gmail.com>
2023-06-04 12:34:29 +0200

    created barebones components

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Text, Group, NavLink, Title, Stack, Button, Paper, Image } from '@mantine/core'
+
+const Navbar = () => {
+  return (
+    <Stack>
+      <Title>
+        Raul Calvo
+      </Title>
+      <Group>
+        <Button>Link 1</Button>
+        <Button>Link 2</Button>
+      </Group>
+    </Stack>
+  )
+}
+
+const Presentation = () => {
+  return (
+    <Paper>
+      <Title>
+        Hola, soy Raul!
+      </Title>
+      <Text>
+        Bienvenido a mi blog, vas a encontrar post sobre lo que voy aprendiendo, enfocado especialmente en desarrollo web y arquitectura web.
+      </Text>
+    </Paper>
+  )
+}
+
+const Doodle = () => {
+  return (
+    <Image src='../assets/avatar.svg' alt='doodle'></Image>
+  )
+}
+
+const PostList = () => {
+  return (
+    <Paper>
+      Esto es una lista de post
+    </Paper>
+  )
+}
+
+
+const Footer = () => {
+  return (
+    <>
+      <Text m={'auto'}>Made by Raul 2023</Text>
+      <Group>
+        <NavLink label='astro' />
+        <NavLink label='mantine' />
+        <NavLink label='github' />
+      </Group>
+    </>
+  )
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Group>
+        <Presentation />
+        <Doodle />
+      </Group>
+      <PostList />
+      <Footer />
     </>
   )
 }
